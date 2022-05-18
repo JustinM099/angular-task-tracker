70ee56c910e26f2c57f0d8da1dfc8ef714e5d889
JustinM099 <justinm099@mac.com>
2022-05-17 23:03:17 -0700

    added button component, added styles, added header component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,10 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  selector: 'app-root', //html tag used to embed component - this is the root. all our components will be embedded into the app component html
+  templateUrl: './app.component.html', //the html file that you're using
+  styleUrls: ['./app.component.css'] //you can have more than one, but this is your styles.
 })
 export class AppComponent {
-  title = 'angular-task-tracker';
+ //any properties or methods of the component.
 }
